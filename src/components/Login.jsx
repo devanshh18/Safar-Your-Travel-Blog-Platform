3b1c219bab7371bcdbe41b6fe7088b4798962c7f
devanshh18 <devanshpatil2202@gmail.com>
2024-09-27 19:11:44 +0530

    update ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center w-full min-h-screen p-4 sm:p-6 lg:p-8">
+    <div className="flex items-center justify-center">
       <Transition
         show={true}
         enter="transition-opacity duration-500"
