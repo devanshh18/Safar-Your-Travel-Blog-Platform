d670a3feed16e4837ad5c0822c56b0ad0f9a85bb
devanshh18 <devanshpatil2202@gmail.com>
2024-09-27 17:24:37 +0530

    debug

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./header/Header";
+import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
