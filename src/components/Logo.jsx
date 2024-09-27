917c689e914346a492662095bd79f6583ff7d444
devanshh18 <devanshpatil2202@gmail.com>
2024-09-27 18:35:52 +0530

    debug

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Logo({ width = "100px" }) {
   return (
-    <img src= "src\assets\logo.png" alt="Website Logo" style={{ width }} />
+    <img src= "\logo.png" alt="Website Logo" style={{ width }} />
   );
 }
 
